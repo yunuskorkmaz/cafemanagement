febeb70cdb57337947bf9aebe9822d8560afd2ef
Yunus Korkmaz <yunuskorkmaz95@gmail.com>
2017-11-20 12:42:15 +0300

    App projesi yolu düzenlendi

--- App/Properties/Resources.Designer.cs
+++ App/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace cafeManagement.Properties
+namespace App.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace cafeManagement.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("cafeManagement.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("App.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
